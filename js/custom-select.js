d05a81a7ebddd9188ef53f49fe97f0f7b1d7e791
Roman Savchyk <romanr301@MacBook-Pro-Roman.local>
2020-02-18 15:35:32 +0200

    filters

--- js/custom-select.js
+++ js/custom-select.js
@@ -10,27 +10,34 @@ for (i = 0; i < x.length; i++) {
   x[i].appendChild(a);
   /*for each element, create a new DIV that will contain the option list:*/
   b = document.createElement("DIV");
+  
   b.setAttribute("class", "select-items select-hide");
   for (j = 1; j < selElmnt.length; j++) {
     /*for each option in the original select element,
     create a new DIV that will act as an option item:*/
     c = document.createElement("DIV");
     c.innerHTML = selElmnt.options[j].innerHTML;
+    
+    // unique = selElemnt.cla
+    // c.setAttribute("class", "1")
     c.addEventListener("click", function(e) {
         /*when an item is clicked, update the original select box,
         and the selected item:*/
         var y, i, k, s, h;
         s = this.parentNode.parentNode.getElementsByTagName("select")[0];
         h = this.parentNode.previousSibling;
+        // unique = this.parentNode.previousSibling.className;
         for (i = 0; i < s.length; i++) {
           if (s.options[i].innerHTML == this.innerHTML) {
             s.selectedIndex = i;
             h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            for (k = 0; k < y.length; k++) {
-              y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
+            h.className = this.className;
+            // h.className = this.unique;
+            // y = this.parentNode.getElementsByClassName("same-as-selected");
+            // for (k = 0; k < y.length; k++) {
+            //   y[k].removeAttribute("class");
+            // }
+            // this.setAttribute("class", "same-as-selected");
             break;
           }
         }
@@ -42,31 +49,64 @@ for (i = 0; i < x.length; i++) {
   a.addEventListener("click", function(e) {
       /*when the select box is clicked, close any other select boxes,
       and open/close the current select box:*/
-      e.stopPropagation();
-      closeAllSelect(this);
+      // e.stopPropagation();
+      // closeAllSelect(this);
       this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
+      // this.classList.toggle("select-arrow-active");
     });
 }
-function closeAllSelect(elmnt) {
-  /*a function that will close all select boxes in the document,
-  except the current select box:*/
-  var x, y, i, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  for (i = 0; i < y.length; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
-    }
-  }
-  for (i = 0; i < x.length; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
+// function closeAllSelect() {
+//   $(document).on('click', '.custom-select', function() {
+//     $(".custom-select div:first-of-type").toggleClass('select-arrow-active')
+//   }) 
+// }
+//   /*a function that will close all select boxes in the document,
+//   except the current select box:*/
+//   var x, y, i, arrNo = [];
+//   x = document.getElementsByClassName("select-items");
+//   y = document.querySelector(".custom-select div:first-of-type");
+//   for (i = 0; i < y.length; i++) {
+//     if (elmnt == y[i]) {
+//       arrNo.push(i)
+//     } else {
+//       y[i].classList.remove("select-arrow-active");
+//     }
+//   }
+//   for (i = 0; i < x.length; i++) {
+//     if (arrNo.indexOf(i)) {
+//       x[i].classList.add("select-hide");
+//     }
+//   }
+// }
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
-document.addEventListener("click", closeAllSelect);
+// document.addEventListener("click", closeAllSelect);
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  $('.select-items div:nth-child(1)').addClass('victoria')
+  $('.select-items div:nth-child(2)').addClass('nsw')
+  $('.select-items div:nth-child(3)').addClass('tasmania')
+  $('.select-items div:nth-child(4)').addClass('quensland')
+  $('.select-items div:nth-child(5)').addClass('newZealand')
+})
+
+$(function() {
+  $(document).on('click', '.custom-select', function() {
+    console.log('1')
+    if ($(".custom-select div:first-of-type").hasClass('select-arrow-active')) {
+      $(".custom-select div:first-of-type").removeClass('select-arrow-active')
+    } else {
+      $(".custom-select div:first-of-type").addClass('select-arrow-active')
+    }
+  }) 
+  $(document).on('click', '.select-items', function() {
+    console.log('1')
+    if ($(".custom-select div:first-of-type").hasClass('select-arrow-active')) {
+      $(".custom-select div:first-of-type").removeClass('select-arrow-active')
+    } else {
+      $(".custom-select div:first-of-type").addClass('select-arrow-active')
+    }
+  }) 
+});
+
